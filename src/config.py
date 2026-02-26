4b12188fb721ee1a3e4790fa14c380c279b3cb98
BOBBYIBM <BobbyChen@ibm.com>
2026-02-26 15:35:54 +0800

    改共用參數的檔案名和路徑

---
+++ src/config.py
@@ -0,0 +1,29 @@
+import os
+from datetime import date
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# PTT 相關配置
+PTT_BASE_URL = "https://www.ptt.cc"
+PTT_COOKIES = {'over18': '1'}
+MAX_PAGE = 500
+START_DATE = date(2026, 1, 2)
+END_DATE = date(2026, 2, 1)
+
+# 圖片相關配置
+IMAGE_BLACKLIST = [
+    "instagram",  
+    "facebook",  
+    "tiktok", 
+    "https://x.com/",  
+    "twitter",
+    "youtube",
+    "https://youtu",
+    "threads"
+]
+
+# Notion 相關配置
+NOTION_SECRET = os.getenv("NOTION_SECRET")
+DATABASE_ID = os.getenv("DATABASE_ID")
+NOTION_CHUNK_SIZE = 100 # Notion API 單次建立 block 的上限
